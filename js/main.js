754242bb5e1ff1732c38334c3664ff34be0a68ed
claudia <clou.7loymed@gmail.com>
2017-01-16 14:06:36 -0500

    password

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ document.getElementById("name").setAttribute("required","");
 document.getElementById("lastname").setAttribute("required","");
 document.getElementById("input-email").setAttribute("required","");
 document.getElementById("input-password").setAttribute("required","");
-document.getElementById("input-password").SetAttribute("minlength","6");
+document.getElementById("input-password").setAttribute("minlength","6");
 
+
+//Name
 function validateM(_evt){
     var name = document.getElementById("name").value;
     
     var nameArray= name.split("");
     var primeraLetra= nameArray[0];
-    var mayuscula= primeraLetra.toUpperCase();
+    var mayuscula = primeraLetra.toUpperCase();
     var espacio= false;
     
     for (var i=1; i<nameArray.length; i++){
@@ -18,24 +20,76 @@ function validateM(_evt){
             espacio =false;
         }else
 			mayuscula += nameArray[i];
-			if(nameArray[i] =="")
+			if(nameArray[i] ==" ")
 			espacio = true;
 					
 			}
 		document.getElementById("name").value= mayuscula;
 }
+
+function validateT(_evt){
+	letra = window.event.keyCode;
+	
+	if((letra>=65 && letra<=90) || letra==8 || letra==32){
+		
+	}else{
+		_evt.preventDefault();
+	}
+}
+
+//lastname
+function validado(_evt){
+    var name = document.getElementById("lastname").value;
     
-
-
-
-//----function validateForm(){
-//    var valor = document.getElementById("input-password").value;
+    var nameArray= name.split("");
+    var primeraLetra= nameArray[0];
+    var mayuscula = primeraLetra.toUpperCase();
+    var espacio= false;
     
-  //  if( valor==="123456" || valor==="098754" || valor==="password"){
-    //    alert('Contraseña no válida');
-      //  return false;
-   // }
-//-- } ---//
+    for (var i=1; i<nameArray.length; i++){
+        if(espacio){
+            mayuscula += nameArray[i].toUpperCase();
+            espacio =false;
+        }else
+			mayuscula += nameArray[i];
+			if(nameArray[i] ==" ")
+			espacio = true;
+					
+			}
+		document.getElementById("lastname").value= mayuscula;
+}
+
+function validadoNumber(_evt){
+	letra = window.event.keyCode;
+	
+	if((letra>=65 && letra<=90) || letra==8 || letra==32){
+		
+	}else{
+		_evt.preventDefault();
+	}
+}
+
+//Email
+
+function validateForm(){
+	var correo= document.getElementById("input-email").value;
+	
+	var expresion= /\w+@\w+\.+[a-z]/;
+	
+	if(!expresion.test(correo)){
+		alert ("Email no válido");
+	}else
+		return false;
+}
+
+function validatePassword(_evt){
+var valor = document.getElementById("input-password").value;
+    
+ if( valor==="123456" || valor==="098754" || valor==="password"){
+      alert('Contraseña no válida');
+      return false;
+  }
+}
 
 
 
